aa5c3570c70a0025473db845b1136a6210a4aada
paulio <pdkm2000@hotmail.com>
2021-03-13 11:35:09 +0000

    Update MonoExtensions.cs

--- Scripts/Helpers/MonoExtensions.cs
+++ Scripts/Helpers/MonoExtensions.cs
@@ -1,15 +1,36 @@
 ﻿namespace Assets.CBC.Scripts.Helpers
 {
+    using System.Runtime.CompilerServices;
     using UnityEngine;
 
     public static class MonoExtensions
     {
-        public static T[] GetComponentsInChildren<T>(this MonoBehaviour monoBehaviour, string nullMessage)
+
+
+        public static T GetComponent<T>(this Component component, string nullMessage)
         {
-            var result = monoBehaviour.GetComponentsInChildren<T>();
-            if (result == null)
+            var result = component.GetComponent<T>();
+            if (result.Equals(null))
             {
-                Debug.LogError(nullMessage);
+                var type = typeof(T);
+                Debug.LogError($"{nullMessage}: Was expecting {type.Name} in {component.name}", component);
+            }
+            else
+            {
+                Debug.Log("It's a null really???" + result.ToString());
+            }
+
+
+            return result;
+        }
+
+        public static T[] GetComponentsInChildren<T>(this Component component, string nullMessage)
+        {
+            var result = component.GetComponentsInChildren<T>();
+            if (result.Equals(null))
+            {
+                var type = typeof(T);
+                Debug.LogError($"{nullMessage}: Was expecting one or more {type.Name} in the children of {component.name}", component);
             }
 
             return result;
